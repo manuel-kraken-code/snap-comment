f42b8af89862083a7d5155bc6c0a088b83d45897
Manuel Escobar III <manuel.esco.20@gmail.com>
2018-04-24 08:42:43 -0600

    added class salty

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+class comment {
+	public function getCommentSaltiness():float {
+		return $this-> commentSaltiness;
+	}
+	public function setCommentSaltiness(float $newCommentSaltiness): bool {
+		// detects bad values - we already know it's float
+		if ($newCommentSaltiness) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	}
+	public function  getCommentText(): string {
+		return $this-> commentText;
+	}
+	public function getCommentUsername(): string {
+		return $this-> commentUsername;
+	}
+}
